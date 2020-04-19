cb6d6e5de6a4a0cd26fbfc29b62309b8fc610f0d
rnovor <rnovor@gmail.com>
2020-04-19 15:04:20 -0500

    Added taskbar interactive controls, added reset and quit functions to window

--- WinFormTaskbarEx/Form1.Designer.cs
+++ WinFormTaskbarEx/Form1.Designer.cs
@@ -28,13 +28,96 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.timerLabel = new System.Windows.Forms.Label();
+            this.timerLabel2 = new System.Windows.Forms.Label();
+            this.buttonRestart = new System.Windows.Forms.Button();
+            this.buttonQuit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(267, 272);
+            this.trackBar1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(362, 45);
+            this.trackBar1.TabIndex = 0;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // timerLabel
+            // 
+            this.timerLabel.AutoSize = true;
+            this.timerLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timerLabel.Location = new System.Drawing.Point(208, 138);
+            this.timerLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.timerLabel.Name = "timerLabel";
+            this.timerLabel.Size = new System.Drawing.Size(105, 16);
+            this.timerLabel.TabIndex = 1;
+            this.timerLabel.Text = "Timer Counter";
+            // 
+            // timerLabel2
+            // 
+            this.timerLabel2.AutoSize = true;
+            this.timerLabel2.Location = new System.Drawing.Point(383, 240);
+            this.timerLabel2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.timerLabel2.Name = "timerLabel2";
+            this.timerLabel2.Size = new System.Drawing.Size(129, 16);
+            this.timerLabel2.TabIndex = 2;
+            this.timerLabel2.Text = "Track Bar Control";
+            // 
+            // buttonRestart
+            // 
+            this.buttonRestart.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonRestart.Location = new System.Drawing.Point(267, 395);
+            this.buttonRestart.Name = "buttonRestart";
+            this.buttonRestart.Size = new System.Drawing.Size(100, 40);
+            this.buttonRestart.TabIndex = 3;
+            this.buttonRestart.TabStop = false;
+            this.buttonRestart.Text = "Restart";
+            this.buttonRestart.UseVisualStyleBackColor = true;
+            this.buttonRestart.Click += new System.EventHandler(this.buttonRestart_Click);
+            // 
+            // buttonQuit
+            // 
+            this.buttonQuit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonQuit.Location = new System.Drawing.Point(529, 395);
+            this.buttonQuit.Name = "buttonQuit";
+            this.buttonQuit.Size = new System.Drawing.Size(100, 40);
+            this.buttonQuit.TabIndex = 4;
+            this.buttonQuit.Text = "Quit";
+            this.buttonQuit.UseVisualStyleBackColor = true;
+            this.buttonQuit.Click += new System.EventHandler(this.buttonQuit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(902, 504);
+            this.Controls.Add(this.buttonQuit);
+            this.Controls.Add(this.buttonRestart);
+            this.Controls.Add(this.timerLabel2);
+            this.Controls.Add(this.timerLabel);
+            this.Controls.Add(this.trackBar1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label timerLabel;
+        private System.Windows.Forms.Label timerLabel2;
+        private System.Windows.Forms.Button buttonRestart;
+        private System.Windows.Forms.Button buttonQuit;
     }
 }
 
--- WinFormTaskbarEx/Form1.cs
+++ WinFormTaskbarEx/Form1.cs
@@ -12,9 +12,99 @@ namespace WinFormTaskbarEx
 {
     public partial class Form1 : Form
     {
+        int timerCounter;
+        int maxValue = 100;
+        Timer PacingTimer = new Timer();
         public Form1()
         {
+            
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            trackBar1.Maximum = maxValue;
+            buttonQuit.Enabled = false;
+            buttonRestart.Enabled = false;
+
+            InitializeTimer(PacingTimer);
+            //PacingTimer.Interval = 1000 * 1; // time in msec 1 sec in this case
+            //PacingTimer.Tick += new EventHandler(PacingTimer_Tick);
+            //PacingTimer.Tag = "FunTimer";
+            //PacingTimer.Start();
+            Run(PacingTimer);
+            //StopTimer(PacingTimer);
+        }
+
+        private void PacingTimer_Tick(object sender, EventArgs e)
+        {
+            if (timerCounter < maxValue)
+            {
+                timerCounter++;
+                timerLabel.Text = @"Current tick count is: " + timerCounter.ToString();
+                //Set the new value on track bar
+                trackBar1.Value = timerCounter;
+                //MessageBox.Show("Time Elapsed!", "Window will be closed");
+                //this.Close();
+            }
+            else
+            {
+                // StopTimer(PacingTimer);
+                PacingTimer.Stop();
+                timerLabel.Text = @"Max Count Reached, Timer " + PacingTimer.Tag + @" now disabled";
+                this.Refresh();
+                buttonQuit.Enabled = true;
+                buttonRestart.Enabled = true;
+           
+            }
+
+        }
+
+        private void InitializeTimer(Timer timerInstance)
+        {
+            timerCounter = 0;
+            timerInstance.Interval = 1000 * 1; // time in msec 1 sec in this case
+            //timerInstance.Tick += new EventHandler(PacingTimer_Tick);
+            timerInstance.Tag = "FunTimer";
+            timerInstance.Start();
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            // Move timer counter based on trackbar user setting
+            timerCounter = trackBar1.Value;
+        }
+
+        private void Run(Timer timerInstance)
+        {
+            timerInstance.Tick += new EventHandler(PacingTimer_Tick);
+        }
+
+        private void ResetForm()
+        {
+            this.Dispose();
+            Application.Restart();
+        }
+
+        private void buttonRestart_Click(object sender, EventArgs e)
+        {
+            ResetForm();
+        }
+
+        private void buttonQuit_Click(object sender, EventArgs e)
+        {
+            Application.ExitThread();
+            //DialogResult dialogBox = MessageBox.Show("Do you really want to exit?");
+            //if (dialogBox == DialogResult.Yes)
+            //{
+            //    Application.ExitThread();
+            //}
+            //else if (dialogBox == DialogResult.No)
+            //{
+            //    ResetForm();
+            //}
+
         }
     }
 }
+
